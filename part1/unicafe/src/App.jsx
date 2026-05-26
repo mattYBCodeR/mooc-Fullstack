5b18a84b8c68d64ed6ebc193d6093705425290a5
mattYBCodeR <166936467+mattYBCodeR@users.noreply.github.com>
2026-05-25 23:20:05 -0400

    Completed unicafe

--- part1/unicafe/src/App.jsx
+++ part1/unicafe/src/App.jsx
@@ -1,90 +1,71 @@
 import { useState } from "react";
 
-const StatisticsLine = ({ text, value }) => {
-  if (text !== "positive") {
-    return (
-      <tr>
-        <td>{text}</td>
-        <td>{value}</td>
-      </tr>
-    )
-  }
-  return (
-    <tr>
-      <td>{text}</td>
-      <td>{value} %</td>
-    </tr>
-  )
-}
-
-const Statistics = ({ good, neutral, bad, total }) => {
-  if (total === 0) {
-    return (
-      <div>
-        <h1> statistics</h1>
-        <p>no feedback given</p>
-      </div>
-    )
+const App = () => {
+  const anecdotes = [
+    "If it hurts, do it more often.",
+    "Adding manpower to a late software project makes it later!",
+    "The first 90 percent of the code accounts for the first 90 percent of the development time...The remaining 10 percent of the code accounts for the other 90 percent of the development time.",
+    "Any fool can write code that a computer can understand. Good programmers write code that humans can understand.",
+    "Premature optimization is the root of all evil.",
+    "Debugging is twice as hard as writing the code in the first place. Therefore, if you write the code as cleverly as possible, you are, by definition, not smart enough to debug it.",
+    "Programming without an extremely heavy use of console.log is same as if a doctor would refuse to use x-rays or blood tests when diagnosing patients.",
+    "The only way to go fast, is to go well.",
+  ];
+  const [selected, setSelected] = useState(0)
+  const [votes, setVotes] = useState({0: 0, 1: 0, 2: 0, 3: 0, 4: 0, 5: 0, 6: 0, 7: 0})
+  // const [votes, setVotes] = useState(Array(anecdotes.length).fill(0))
+  const handleRandomAnecdote = () => {
+    const randomIndex = Math.floor(Math.random() * anecdotes.length);
+    setSelected(randomIndex);
+    console.log(randomIndex);
   }
 
-  const average = total === 0 ? 0 : (good - bad) / total;
-  const positive = total === 0 ? 0 : (good / total) * 100;
+  const handleVoteClick = () => {
+    const newVotes = {...votes}
+    console.log(newVotes)
+    newVotes[selected] += 1
+    setVotes(newVotes)
+  }
+
+  const getAnecdoteWithMostVotes = () => {
+    const indexWithMostVotes = Object.keys(votes).find(key => votes[key] === Math.max(...Object.values(votes)))
+    return anecdotes[indexWithMostVotes]
+  }
+
+  const getMostVotes = () => Math.max(...Object.values(votes))
 
   return (
     <div>
-      <h1> statistics</h1>
+      <h1> Anecdotes of the day</h1>
       <table>
         <tbody>
-          <StatisticsLine text="good" value={good} />
-          <StatisticsLine text="neutral" value={neutral} />
-          <StatisticsLine text="bad" value={bad} />
-          <StatisticsLine text="total" value={total} />
-          <StatisticsLine text="average" value={average} />
-          <StatisticsLine text="positive" value={positive} unit=" %" />
+          <tr>
+            <td>{anecdotes[selected]}</td>
+          </tr>
+          <tr>
+            <td>has {votes[selected]} votes</td>
+          </tr>
+          <tr>
+            <td>
+              <button onClick={handleRandomAnecdote}>Next Anecdote</button>
+              <button onClick={handleVoteClick}>Vote</button>
+            </td>
+          </tr>
+        </tbody>
+      </table>
+      <h1> Anecdote with the most votes</h1>
+      <table>
+        <tbody>
+        <tr>
+          <td>{getAnecdoteWithMostVotes()}</td>
+        </tr>
+        <tr>
+          <td>{getMostVotes()}</td>
+        </tr>
         </tbody>
       </table>
     </div>
-  )
-}
-
-const Button = ({ onClick, text }) => {
-  return <button onClick={onClick}> {text}</button>;
-}
-
-const App = () => {
-  // save clicks of each button to its own state
-  const [good, setGood] = useState(0);
-  const [neutral, setNeutral] = useState(0);
-  const [bad, setBad] = useState(0);
-  const [total, setTotal] = useState(0);
-
-  const hanldeIsGood = () => {
-    console.log("Increasing good to", good + 1);
-    setGood(good + 1);
-    setTotal(total + 1);
-  };
-
-  const hanldeIsBad = () => {
-    console.log("Increasing bad to", bad + 1);
-    setBad(bad + 1);
-    setTotal(total + 1);
-  };
-
-  const hanldeIsNeutral = () => {
-    console.log("Increasing neutral to", neutral + 1);
-    setNeutral(neutral + 1);
-    setTotal(total + 1);
-  };
-
-  return (
-    <div>
-      <h1>give feedback</h1>
-      <Button onClick={hanldeIsGood} text="good" />
-      <Button onClick={hanldeIsNeutral} text="neutral" />
-      <Button onClick={hanldeIsBad} text="bad" />
-      <Statistics good={good} bad={bad} neutral={neutral} total={total} />
-    </div>
-  )
-}
+  );
+};
 
 export default App;
